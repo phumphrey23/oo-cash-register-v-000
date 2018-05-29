fb4c8200b27658f235bd430c33d8ffb217f8abd0
Precious Humphrey <phumphrey23@gmail.com>
2018-05-29 02:13:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+attr_accessor :total :cash_register :cash_register_with_discount
 
+def initialize()
+  @total = 0
+  @discount
+end
+
+def total
+  @total
+end
+
+def add_item(title, price)
+  @total + price
+end
+
+def apply_discount
+
+end
+
+def items
+  
+end
+
+def void_last_transaction
+  
+end
+
+end
