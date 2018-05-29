69212dbda3ced6292bb3327b15fa3042fe020988
Precious Humphrey <phumphrey23@gmail.com>
2018-05-29 02:48:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ def add_item(title, price, quantity = 1)
 end
 
 def apply_discount
-   if discount = 0
+   if discount == 0
     puts "There is no discount to apply"
    else
     @total = @total*(1-@discount/100)
